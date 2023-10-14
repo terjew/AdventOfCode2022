c7bc671d8fc4db8f995ba31751e58a3ad4600f82
Terje Wiesener <terje.wiesener@iverdi.no>
2023-10-14 23:22:20 +0200

    Added C# solution for day1 and F# solution for day9(p1)

---
+++ Day1/Day1.cs
@@ -0,0 +1,47 @@
+﻿namespace Day1
+{
+    internal class Day1
+    {
+        internal class Elf
+        {
+            public List<int> Rations = new List<int>();
+            public int Total = 0;
+            public static List<Elf> ParseAll(string path)
+            {
+                var lines = File.ReadAllLines("Input.txt");
+                List<Elf> elves = new List<Elf>();
+                Elf elf = new Elf();
+                foreach (var line in lines)
+                {
+                    if (string.IsNullOrEmpty(line))
+                    {
+                        elves.Add(elf);
+                        elf = new Elf();
+                        continue;
+                    }
+                    var ration = int.Parse(line);
+                    elf.Rations.Add(ration);
+                    elf.Total += ration;
+                }
+                return elves;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var elves = Elf.ParseAll("Input.txt");
+            Console.WriteLine($"Most total calories: {Part1(elves)}");
+            Console.WriteLine($"Top 3 most total calories: {Part2(elves)}");
+        }
+
+        static int Part1(IEnumerable<Elf> elves)
+        {
+            return elves.Max(e => e.Total);
+        }
+
+        static int Part2(IEnumerable<Elf> elves)
+        {
+            return elves.OrderByDescending(e => e.Total).Take(3).Sum(e => e.Total);
+        }
+    }
+}
---
+++ Day9/Day9.fs
@@ -0,0 +1,98 @@
+﻿open System
+open System.IO
+
+type Vector = 
+    { 
+        X: int; 
+        Y: int;
+    }
+
+module Vector =
+    let U = {X =  0; Y =  1}
+    let D = {X =  0; Y = -1}
+    let L = {X = -1; Y = 0}
+    let R = {X =  1; Y = 0}
+
+    let abs (vec:Vector) : Vector =
+        {
+            X = Math.Abs vec.X; 
+            Y = Math.Abs vec.Y
+        }
+
+    let clamp (vec:Vector) : Vector = 
+        {
+            X = Math.Clamp(vec.X, -1, 1);
+            Y = Math.Clamp(vec.Y, -1, 1)
+        }
+
+    let max (vec:Vector) : int =
+        let v1 = abs vec
+        Math.Max (v1.X, v1.Y)
+
+type Position = 
+    { 
+        X: int; 
+        Y: int;
+    }
+
+    static member (-) ((posa : Position), (posb : Position)) : Vector =
+        {X = posa.X - posb.X; Y = posa.Y - posb.Y}
+
+    static member (+) ((pos : Position), (vec: Vector)) : Position =
+        {X = pos.X + vec.X; Y = pos.Y + vec.Y}
+
+module Position = 
+    let move (offset : Vector) (position : Position) =
+        position + offset
+
+type Rope(head : Position, tail : Position, history : Set<Position>) = 
+    member this.Head with get() = head
+    member this.Tail with get() = tail
+    member this.History with get() = history.Add tail
+
+module Rope =
+    let CalculateTail (head:Position) (tail:Position) : Position =
+        let vec = head - tail
+        let max = Vector.max vec
+        match max with
+            | 0 -> tail
+            | 1 -> tail
+            | _ -> tail + Vector.clamp vec
+
+    let Move (moveFunc:Position->Position)  (rope : Rope) : Rope =
+        let h1 = moveFunc(rope.Head)
+        let t1 = CalculateTail h1 rope.Tail
+        Rope(h1, t1, rope.History)
+
+let readLines filePath = 
+    File.ReadLines(filePath)
+
+let notEmpty (s:string) = 
+    String.IsNullOrEmpty(s) = false
+
+let getMove (operation:String) : (Rope->Rope) =
+    match operation with
+        | "U" -> Rope.Move (Position.move Vector.U)
+        | "D" -> Rope.Move (Position.move Vector.D)
+        | "L" -> Rope.Move (Position.move Vector.L)
+        | "R" -> Rope.Move (Position.move Vector.R)
+        | _ -> failwith "wtf"
+
+let getMoves (str:String) : seq<(Rope->Rope)> =
+    let segments = str.Split " "
+    let operation = segments[0]
+    let count = int segments[1]
+    let move = getMove operation
+    Seq.replicate count move 
+
+let solve path =
+    let p0 = {X = 0; Y = 0}
+    let initialRope = Rope(p0, p0, Set<Position>([]))
+    readLines path
+    |> Seq.filter notEmpty
+    |> Seq.collect getMoves
+    |> Seq.fold (fun rope moveFunc -> moveFunc rope) initialRope
+
+let rope = solve "input.txt"
+
+printfn "Number of positions visited: %A" rope.History.Count
